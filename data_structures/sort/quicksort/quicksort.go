b44091b4edca2a2dfe99b9201069dc94579927a4
xiaobowen <xiaobowen@bytedance.com>
2020-07-30 22:33:59 +0800

    qs 2

--- data_structures/sort/quicksort/quicksort.go
+++ data_structures/sort/quicksort/quicksort.go
@@ -40,20 +40,47 @@ func QuickSortInts(arr []int) {
 	}
 	key := arr[0]
 	for l < r {
-		for l < r && arr[l] <= key {
-			l++
-		}
-		
 		for l < r && arr[r] >= key {
 			r--
 		}
+		if l < r {
+			arr[l] = arr[r]
+		}
+
+		for l < r && arr[l] <= key {
+			l++
+		}
 
 		if l < r {
-			arr[l], arr[r] = arr[r], arr[l]
+			arr[r] = arr[l]
 		}
+
 	}
 
 	arr[l] = key
+	QuickSortInts(arr[:l])
+	QuickSortInts(arr[l+1:])
+	
+	
+}
+
+func QuickSortInts2(arr []int) {
+	
+	l := 0
+	r := len(arr) - 1
+	
+	if len(arr) < 2 {
+		return
+	}
+	key := arr[r]
+	for i := range arr {
+		if arr[i] < key {
+			arr[i], arr[l] = arr[l], arr[i]
+			l++
+		}
+	}
+
+	arr[l], arr[r] = arr[r], arr[l]
 	QuickSortInts(arr[:l])
 	QuickSortInts(arr[l+1:])
 	
